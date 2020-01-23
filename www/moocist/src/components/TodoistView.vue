54678f91086a4f1370af3d29f618ea3132bcbac4
Łukasz Hryniuk <code@hryniuk.pl>
2020-01-23 23:34:46 +0100

    Prettify task list

--- www/moocist/src/components/TodoistView.vue
+++ www/moocist/src/components/TodoistView.vue
@@ -1,35 +1,55 @@
 <template>
-    <v-container fluid fill-height class="todist-view">
-        <v-layout justify-right align-left column pa-5>
-            <v-list-item-group>
-            <v-list-item
-              v-for="item in items"
-              :key="item"
-            >
-              <v-list-item-content>
-                <v-list-item-title v-text="item"></v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-            </v-list-item-group>
-        </v-layout>
-    </v-container>
+  <v-container fluid fill-height class="todist-view">
+    <v-layout justify-right align-left column pa-5>
+      <v-card max-width="600" class="mx-auto">
+        <v-toolbar color="red" dark>
+          <v-app-bar-nav-icon></v-app-bar-nav-icon>
+          <v-toolbar-title>Todoist Preview</v-toolbar-title>
+          <v-spacer></v-spacer>
+        </v-toolbar>
+
+        <v-list two-line>
+          <v-list-item-group>
+            <template v-for="item in items">
+              <v-list-item :key="item.title">
+                <template>
+                  <v-btn icon>
+                    <v-icon>mdi-checkbox-blank-circle-outline</v-icon>
+                  </v-btn>
+
+                  <v-list-item-content>
+                    <v-list-item-title v-text="item"></v-list-item-title>
+                    <!-- <v-list-item-subtitle class="text--primary" v-text="item.headline"></v-list-item-subtitle>
+                    <v-list-item-subtitle v-text="item.subtitle"></v-list-item-subtitle> -->
+                  </v-list-item-content>
+                </template>
+              </v-list-item>
+
+              <!-- <v-divider v-if="index + 1 < items.length" :key="index"></v-divider> -->
+            </template>
+          </v-list-item-group>
+        </v-list>
+      </v-card>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
-    name: 'TodoistView',
-    data: () => ({
-        items: ['a', 'b', 'c'],
-        count: 3,
-    })
+  name: "TodoistView",
+  data: () => ({
+    title: "Course Title",
+    items: ["Video 1", "Video 2", "Video 3"],
+    count: 3
+  })
 };
 </script>
 
 <style scoped>
 .todoist-view {
-    background-size: cover;
-    background-color: red;
-    width: 100%;
-    height: 100%;
+  background-size: cover;
+  background-color: red;
+  width: 100%;
+  height: 100%;
 }
 </style>
