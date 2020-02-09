ce77926efaaa3ffcdd26fc62de30ea24830c3ec8
Łukasz Hryniuk <code@hryniuk.pl>
2020-02-09 17:26:27 +0100

    Implement syllabus validation

--- pkg/mooc/mooc.go
+++ pkg/mooc/mooc.go
@@ -1,5 +1,11 @@
 package mooc
 
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+)
+
 type Task struct {
 	Title    string `json:"title"`
 	Duration string `json:"duration"`
@@ -14,10 +20,45 @@ type CourseSyllabus struct {
 	Weeks []Week `json:"weeks"`
 }
 
+func (cs CourseSyllabus) Validate() error {
+	if len(cs.Weeks) == 0 {
+		return errors.New("empty weeks")
+	}
+
+	for i, week := range cs.Weeks {
+		if len(week.Title) == 0 {
+			return fmt.Errorf("empty title in week with index %d", i)
+		}
+
+		if len(week.Tasks) == 0 {
+			return fmt.Errorf("empty tasks in week with index %d", i)
+		}
+
+		for j, task := range week.Tasks {
+			if len(task.Title) == 0 {
+				return fmt.Errorf("empty title in task with index %d in week index %d", j, i)
+			}
+
+			if len(task.Duration) == 0 {
+				return fmt.Errorf("empty duration in task with index %d in week index %d", j, i)
+			}
+		}
+	}
+
+	return nil
+}
+
 type Importer interface {
 	Import() (CourseSyllabus, error)
 }
 
 type Exporter interface {
-	Export(CourseSyllabus) ([]byte, error)
+	Export(cs CourseSyllabus) ([]byte, error)
+}
+
+type JsonExporter struct {
+}
+
+func (e *JsonExporter) Export(cs CourseSyllabus) ([]byte, error) {
+	return json.Marshal(cs)
 }
