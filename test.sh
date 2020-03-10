5ade5ad4d5418539be95e651221339cd0272040b
Łukasz Hryniuk <code@hryniuk.pl>
2020-03-10 23:38:59 +0100

    Add more test slugs and test results

--- test.sh
+++ test.sh
@@ -3,10 +3,11 @@
 for i in `cat test_slugs.txt`; do
     ./moocist template --coursera-slug $i &> /dev/null
     res=$?
+    url="https://www.coursera.org/learn/${i}"
     if [ $res -eq 0 ] ; then
-        echo -e "${i}\t\033[32mOK\033[0m"
+        echo -e "${url}\t\033[32mOK\033[0m"
     else
-        echo -e "${i}\t\033[31mNOK\033[0m"
+        echo -e "${url}\t\033[31mNOK\033[0m"
 
     fi
 done
