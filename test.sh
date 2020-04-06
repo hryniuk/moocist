6f3c5fa9056bd42d7f75f5ffe28f1529cdd8704b
Łukasz Hryniuk <code@hryniuk.pl>
2020-04-06 20:53:19 +0200

    Add status code to test script

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+failed=0
+
 for i in `cat test_slugs.txt`; do
     ./moocist template --coursera-slug $i &> /dev/null
     res=$?
@@ -8,6 +10,10 @@ for i in `cat test_slugs.txt`; do
         echo -e "${url}\t\033[32mOK\033[0m"
     else
         echo -e "${url}\t\033[31mNOK\033[0m"
-
+        failed=1
     fi
 done
+
+if [ $failed -eq 1 ] ; then
+    exit 1
+fi
